e5fca631db28e3fd0e218e1943ad100e556ef838
gregH <greg.henry@mail.com>
2021-10-13 09:38:33 +0200

    Updates according to template library

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.1.2] - 2021-10-13
+
+### Changed
+
++ updates according to library template
+
 ## [0.1.1] - 2021-09-22
 
 ### Added
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}.py
@@ -4,25 +4,25 @@
 {{cookiecutter.project_name}}.
 """
 
-from pathlib import Path  # noqa: F401
+from pathlib import Path
 from typing import Optional  # noqa: F401
 
-import ruamel.yaml  # noqa: F401
-from dotmap import DotMap  # noqa: F401
+import ruamel.yaml
+from dotmap import DotMap
 from pudb import set_trace as bp  # noqa: F401
 
 VERSION = "0.1.0"
 CONFIG_FILE = Path("config.yaml")
 
 
-def read_config(FILE_PATH: Path) -> DotMap:
+def read_yaml(FILE_PATH: Path) -> DotMap:
     """Load the config file."""
     CFG, IND, BSI = ruamel.yaml.util.load_yaml_guess_indent(FILE_PATH.open())
     return DotMap(CFG)
 
 
 def main() -> None:
-    CFG = read_config(CONFIG_FILE)  # noqa: F841
+    CFG = read_yaml(CONFIG_FILE)  # noqa: F841
 
 
 if __name__ == "__main__":
